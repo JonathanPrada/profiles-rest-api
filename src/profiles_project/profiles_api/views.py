6d887ea1834db0944dca741c2320cb1f12dd7644
Jonathan Prada <jonathanprada@protonmail.ch>
2017-08-22 14:08:46 +0200

    Added Post, Put, Patch and Delete functions to our API view

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
 
 # Create your views here.
 class HelloAPIView(APIView):
     """Test API View"""
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of API view features"""
@@ -18,3 +22,32 @@ class HelloAPIView(APIView):
 
         # A response object must be part of a dictionary to return the response
         return Response({'message': 'hello!', 'an apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+
+        # Pass in the request data to the data attribute
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        # validate the data, the info stored in a dictionary called data
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            # Create a message and format the string to include name var
+            message = 'Hello {0}'.format(name)
+            # Pass the response as an object
+            return Response({'message': message})
+        else:
+            # Errors contains all the error that happened during validation
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Handles updating the whole object"""
+        return Response({'method': 'put'})
+
+    def patch(self, request, pk=None):
+        """Handles partially updating some fields of the object"""
+        return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None):
+        """Handles deleting an object"""
+        return Response({'method': 'delete'})
