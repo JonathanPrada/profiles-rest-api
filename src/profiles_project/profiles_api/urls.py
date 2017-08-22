b88000279c8e895900519f31c688a8d93443c9eb
Jonathan Prada <jonathanprada@protonmail.ch>
2017-08-22 17:24:53 +0200

    Added login API

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
-router.register('profile', views.UserProfileViewSet) # django auto sets basename
+router.register('profile', views.UserProfileViewSet) # auto basename for models
+router.register('login', views.LoginViewSet, base_name='login')
 
 urlpatterns = [
     url(r'^hello-view/', views.HelloAPIView.as_view()),
